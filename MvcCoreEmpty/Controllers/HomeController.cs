c80cf8041460b4f7beeb1acdc954a602eab60108
mmtol <mmigueltolosana@gmail.com>
2026-01-29 09:43:41 +0100

    fin

--- MvcCoreEmpty/Controllers/HomeController.cs
+++ MvcCoreEmpty/Controllers/HomeController.cs
@@ -8,5 +8,25 @@ namespace MvcCoreEmpty.Controllers
         {
             return View();
         }
+
+        public IActionResult Vista1()
+        {
+            return View();
+        }
+
+        public IActionResult Vista2()
+        {
+            return View();
+        }
+
+        public IActionResult VistaPersona()
+        {
+            Persona persona = new Persona();
+            persona.Nombre = "Pepa";
+            persona.Email = "pepa@email.com";
+            persona.Edad = 20;
+
+            return View(persona);
+        }
     }
 }
---
+++ MvcCoreEmpty/Models/Persona.cs
@@ -0,0 +1,6 @@
+﻿public class Persona
+{
+    public string Nombre { get; set; }
+    public string Email { get; set; }
+    public int Edad { get; set; }
+}
