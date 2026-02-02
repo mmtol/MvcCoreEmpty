7af5ea3957b3f1b8977488c8cd08aa03ef2ada27
mmtol <mmigueltolosana@gmail.com>
2026-02-02 10:45:59 +0100

    enfermo

---
+++ MvcCoreLinqToSql/MvcCoreLinqToSql/Controllers/EnfermosController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreLinqToSql.Repositories;
+using MvcCoreLinqToSql.Models;
+
+namespace MvcCoreLinqToSql.Controllers
+{
+    public class EnfermosController : Controller
+    {
+        private RepositoryEnfermos repo;
+
+        public EnfermosController()
+        {
+            repo = new RepositoryEnfermos();
+        }
+
+        public IActionResult Index()
+        {
+            List<Enfermo> enfermos = repo.GetEnfermos();
+            return View(enfermos);
+        }
+
+        public IActionResult Details(string inscripcion)
+        {
+            Enfermo enfermo = repo.FindEnfermo(inscripcion);
+            return View(enfermo);
+        }
+
+        public IActionResult Delete(string inscripcion)
+        {
+            repo.DeleteEnfermo(inscripcion);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MvcCoreLinqToSql/MvcCoreLinqToSql/Repositories/RepositoryEnfermos.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Data.SqlClient;
+using MvcCoreLinqToSql.Models;
+using System.Data;
+
+namespace MvcCoreLinqToSql.Repositories
+{
+    public class RepositoryEnfermos
+    {
+        private SqlConnection conn;
+        private SqlCommand command;
+
+        private DataTable tablaEnfermos;
+
+        public RepositoryEnfermos()
+        {
+            string stringConn = @"Data Source=LOCALHOST\DEVELOPER;Initial Catalog=HOSPITAL;Persist Security Info=True;User ID=SA;Encrypt=True;Trust Server Certificate=True";
+
+            conn = new SqlConnection(stringConn);
+            command = new SqlCommand();
+
+            string sql = "select * from ENFERMO";
+            SqlDataAdapter ad = new SqlDataAdapter(sql, stringConn);
+            tablaEnfermos = new DataTable();
+            ad.Fill(tablaEnfermos);
+        }
+
+        private void InicializarCommand(string sql)
+        {
+            command.Connection = conn;
+            command.CommandType = CommandType.Text;
+            command.CommandText = sql;
+        }
+
+        public List<Enfermo> GetEnfermos()
+        {
+            var consulta = from datos in tablaEnfermos.AsEnumerable()
+                           select datos;
+            List<Enfermo> enfermos = new List<Enfermo>();
+            foreach (var fila in consulta)
+            {
+                Enfermo e = new Enfermo();
+                e.Inscripcion = fila.Field<string>("INSCRIPCION");
+                e.Apellido = fila.Field<string>("APELLIDO");
+                e.Direccion = fila.Field<string>("DIRECCION");
+                e.FechaNac = fila.Field<DateTime>("FECHA_NAC");
+                e.Sexo = fila.Field<string>("S");
+                e.Nss = fila.Field<string>("NSS");
+                enfermos.Add(e);
+            }
+
+            return enfermos;
+        }
+
+        public Enfermo FindEnfermo(string inscripcion)
+        {
+            var consulta = from datos in tablaEnfermos.AsEnumerable()
+                           where datos.Field<string>("INSCRIPCION") == inscripcion
+                           select datos;
+            var fila = consulta.First();
+
+            Enfermo e = new Enfermo();
+            e.Inscripcion = fila.Field<string>("INSCRIPCION");
+            e.Apellido = fila.Field<string>("APELLIDO");
+            e.Direccion = fila.Field<string>("DIRECCION");
+            e.FechaNac = fila.Field<DateTime>("FECHA_NAC");
+            e.Sexo = fila.Field<string>("S");
+            e.Nss = fila.Field<string>("NSS");
+
+            return e;
+        }
+
+        public int DeleteEnfermo(string inscripcion)
+        {
+            int registros = 0;
+
+            string sql = "Delete from ENFERMO where INSCRIPCION = @ins";
+            command.Parameters.Clear();
+            SqlParameter paramIns = new SqlParameter("@ins", inscripcion);
+            command.Parameters.Add(paramIns);
+
+            InicializarCommand(sql);
+            conn.Open();
+            registros = command.ExecuteNonQuery();
+
+            conn.Close();
+
+            return registros;
+        }
+    }
+}
